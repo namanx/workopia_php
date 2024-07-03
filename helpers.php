856cbb1f72820e2ef10a8d518fb3d9d9e3c6a1fe
Naman <namanwinner5@gmail.com>
2024-07-03 14:05:01 +0530

    Initialization of the project

---
+++ helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * Get the base path 
+ * 
+ * @param string $path
+ * @return string
+ */
+
+function basePath($path = ''){
+    return __DIR__ . '\\' . $path;
+
+}
+ 
+?>
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+
+require('../helpers.php');
+// require('../views/home.view.php');
+require(basePath('views\home.view.php'));
+?>
